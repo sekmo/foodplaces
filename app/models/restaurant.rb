9aff90fd5fbe6916d46339e15a687e7884ff25cf
Francesco Mari <francescojjmari@gmail.com>
2017-11-02 10:32:49 +0100

    dishes nested attributes in Restaurant

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,6 +1,8 @@
 class Restaurant < ApplicationRecord
+  has_many :dishes, dependent: :destroy
+  accepts_nested_attributes_for :dishes
+
   validates :name, presence: true
   validates :description, presence: true
   validates :address, presence: true
-  has_many :dishes
 end
